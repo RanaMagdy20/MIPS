780fb7bf2fd665f87448f8516d1bb5f8c9ed356e
RAANM <105028031+RanaMagdy20@users.noreply.github.com>
2024-09-17 03:52:24 +0300

    Update TOP_TB.v

--- TOP_TB.v
+++ TOP_TB.v
@@ -1,27 +1,26 @@
 module TOP_TB ();
 parameter TCLK=10;
-parameter DATA_WIDTH=32 , INSTR_MEM_DEPTH=32, Data_MEM_DEPTH=32, REG_DEPTH=32;
+parameter DATA_WIDTH=32 , INSTR_MEM_DEPTH=4096, Data_MEM_DEPTH=4096, REG_DEPTH=32;
 reg CLK,RST,WE;
-//reg [DATA_WIDTH-1:0] INSTRUCTIONS;
+reg [DATA_WIDTH-1:0] INSTRUCTIONS;
+wire [DATA_WIDTH-1:0] s0;
 integer i;
 //clock generation:
 always #(TCLK/2) CLK=~CLK;
-
-//INSTantiation:
 
 TOP #(.DATA_WIDTH(DATA_WIDTH), .INSTR_MEM_DEPTH(INSTR_MEM_DEPTH), .Data_MEM_DEPTH(Data_MEM_DEPTH), .REG_DEPTH(REG_DEPTH)) DUT 
 (
 .CLK(CLK),
 .WE(WE),
-.RST(RST)//,
-//.INSTRUCTIONS(INSTRUCTIONS)
+.RST(RST),
+.INSTRUCTIONS(INSTRUCTIONS),
+.s0(s0)
 );
 
-initial begin
-  //load instructions:
-    $readmemh("memm.txt",DUT.Instr_mem.mem);
 
-  // load registers:
+
+initial begin
+
   for (i=0; i<REG_DEPTH;i=i+1)
  DUT.RF.mem[i] = i;
 
@@ -31,9 +30,9 @@ initial begin
     #TCLK;
     RST=0;
     #TCLK;
-    //WE=1;
     RST=1;
-    #100 $stop;
+    $readmemh("memm.txt",DUT.Instr_mem.mem);
+    #(100*TCLK) $stop;
 
 end
 endmodule
